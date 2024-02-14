fa15a2704bb839851c70d5f65d775ff67aef2461
Harrison Hank <codinghank@gmail.com>
2024-02-15 00:52:41 +0300

    changes on the form file

--- virtual/ecom/store/views.py
+++ virtual/ecom/store/views.py
@@ -43,15 +43,15 @@ def register_user(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            password = form.cleaned_data['password1']
 
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, "Account was created for " + username)
-            return redirect('login')
+            return redirect('home')
         else:
             messages.success(request, "Error creating account")
-            return redirect('home')
+            return redirect('register')
     else:
         return render(request, 'register.html', {'form': form})
         # username = request.POST['username']
