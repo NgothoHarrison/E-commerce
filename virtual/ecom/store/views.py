bd5b905085912ff4fc05b2b1012f3b0584062c98
Harrison Hank <codinghank@gmail.com>
2024-02-14 23:24:32 +0300

    login function

--- virtual/ecom/store/views.py
+++ virtual/ecom/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -9,10 +9,25 @@ def home(request):
     return render(request, 'home.html', {'products': products})
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {})
 
 def login_user(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Error logging in - please try again")
+            return redirect('login')
+
+
+    return render(request, 'login.html', {})
 
 def logout_user(request):
-    return render(request, 'logout.html')
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
