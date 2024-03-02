80dd8b21d55611602f5e004f4fc8b87da06ca241
Harrison Hank <codinghank@gmail.com>
2024-03-02 21:44:47 +0300

    delete from cart

--- virtual/ecom/cart/cart.py
+++ virtual/ecom/cart/cart.py
@@ -56,6 +56,13 @@ class Cart():
         self.cart[product_id] = product_qty
         self.session.modified = True
 
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.session.modified = True
+            
+
 
 
 
