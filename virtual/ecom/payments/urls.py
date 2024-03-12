91b7c4ddab2d41261f817453c86810a23c46741e
Harrison Hank <codinghank@gmail.com>
2024-03-12 09:48:28 +0300

    Urls update

--- virtual/ecom/payments/urls.py
+++ virtual/ecom/payments/urls.py
@@ -2,15 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
-    path('update_user/', views.update_user, name='update_user'),
-    path('update_info/', views.update_info, name='update_info'),
-    path('update_password/', views.update_password, name='update_password'),
-    path('product/<int:pk>', views.product, name='product'),
-    path('category/<str:foo>', views.category, name='category'),
-    path('search/', views.search, name='search'),
+    path('payment_success/', views.payments_success, name= 'payment_success'),
 ]
