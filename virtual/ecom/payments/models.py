a6cf0077ef0f8d2ecab40f311a918b12486a252f
Harrison Hank <codinghank@gmail.com>
2024-03-12 21:28:00 +0300

    shipping address ```

--- virtual/ecom/payments/models.py
+++ virtual/ecom/payments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# shipping models
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE, null=True, blank=True)
+    fullname = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=200, null=False)
