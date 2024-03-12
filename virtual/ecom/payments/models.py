1919d57dc0210843001b1785cfdc989119bfda1f
Harrison Hank <codinghank@gmail.com>
2024-03-12 21:37:18 +0300

    Register the shippingaddress model

--- virtual/ecom/payments/models.py
+++ virtual/ecom/payments/models.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import User
 # shipping models
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE, null=True, blank=True)
-    fullname = models.CharField(max_length=200, null=False)
+    full_name = models.CharField(max_length=200, null=False)
     email = models.EmailField(max_length=200, null=False)
-    
+    address1 = models.CharField(max_length=200, null=False)
+    address2 = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=False)
+    pickup_point = models.CharField(max_length=200, null=False)
+
+    # avoid prular model in admin panel
+
+    class Meta:
+        verbose_name_plural = 'Shipping Addresses'
+
+        def __str__(self) -> str:
+            return f'Shipping Address - {str(self.id)}'
+
