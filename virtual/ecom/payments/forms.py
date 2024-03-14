937cc7b7fac40e2173862d07d8a4d91aa3df549d
Harrison Hank <codinghank@gmail.com>
2024-03-14 12:56:07 +0300

    shipping form

--- virtual/ecom/payments/forms.py
+++ virtual/ecom/payments/forms.py
@@ -2,5 +2,14 @@ from django import forms
 from .models import ShippingAddress
 
 class ShippingAddressForm(forms.ModelForm):
-    class Meta:
-        
+    shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), required=True)
+
+
+
+    shipping_full_name = models.CharField(max_length=200, null=False)
+    shipping_email = models.EmailField(max_length=200, null=False)
+    shipping_address1 = models.CharField(max_length=200, null=False)
+    shipping_address2 = models.CharField(max_length=200, null=True, blank=True)
+    shipping_city = models.CharField(max_length=200, null=False)
+    pickup_point = models.CharField(max_length=200, null=False)
+       
--- virtual/ecom/payments/models.py
+++ virtual/ecom/payments/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # shipping models
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE, null=True, blank=True)
-    full_name = models.CharField(max_length=200, null=False)
-    email = models.EmailField(max_length=200, null=False)
-    address1 = models.CharField(max_length=200, null=False)
-    address2 = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200, null=False)
+    shipping_full_name = models.CharField(max_length=200, null=False)
+    shipping_email = models.EmailField(max_length=200, null=False)
+    shipping_address1 = models.CharField(max_length=200, null=False)
+    shipping_address2 = models.CharField(max_length=200, null=True, blank=True)
+    shipping_city = models.CharField(max_length=200, null=False)
     pickup_point = models.CharField(max_length=200, null=False)
 
     # avoid prular model in admin panel
